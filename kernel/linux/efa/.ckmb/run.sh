802bfef95bca8f7f87c513f816fc19e176e1d7b2
Alejandro Guijarro Monerris <alejandro.guijarromonerris@datadoghq.com>
2026-03-03 16:32:36 +0100

    [local] feat(ckmb): add complete run.sh

--- kernel/linux/efa/.ckmb/run.sh
+++ kernel/linux/efa/.ckmb/run.sh
@@ -1,3 +1,41 @@
 #!/bin/bash -e
 
-modprobe -b /opt efa
+# efa needs ib_core and ib_uverbs to be loaded
+# assume it is mounted at /lib/modules (defualt path for modprobe)
+
+_unload_driver() {
+    local efa_refs=0
+
+    if [ -f /sys/module/efa/refcnt ]; then
+        efa_refs=$(< /sys/module/efa/refcnt)
+    fi
+
+    if [ ${efa_refs} -gt 0 ]; then
+        # run lsmod to debug module usage
+        lsmod | grep efa
+        echo "Could not unload efa kernel module, module is in use" >&2
+        return 1
+    fi
+
+    rmmod efa
+    return 0
+}
+
+install() {
+    echo "Unloading efa kernel module"
+    _unload_driver || exit 1
+
+    echo "Loading efa kernel module"
+    modprobe ib_core
+    modprobe ib_uverbs
+    modprobe -d /opt efa || exit 1
+
+    echo "Done, now waiting for signal"
+    sleep infinity &
+    trap "echo 'Caught signal'; _unload_driver && { kill $!; exit 0; }" HUP INT QUIT PIPE TERM
+    trap - EXIT
+    while true; do wait $! || continue; done
+    exit 0
+}
+
+install
--- kernel/linux/efa_nv_peermem/.ckmb/run.sh
+++ kernel/linux/efa_nv_peermem/.ckmb/run.sh
@@ -1,3 +1,43 @@
 #!/bin/bash -e
 
-modprobe -b /opt efa_nv_peermem
+# efa_nv_peermem needs nvidia to be loaded
+
+_unload_driver() {
+    local efa_nv_peermem_refs=0
+
+    if [ -f /sys/module/efa_nv_peermem/refcnt ]; then
+        efa_nv_peermem_refs=$(< /sys/module/efa_nv_peermem/refcnt)
+    fi
+
+    if [ ${efa_nv_peermem_refs} -gt 0 ]; then
+        # run lsmod to debug module usage
+        lsmod | grep efa_nv_peermem
+        echo "Could not unload efa_nv_peermem kernel module, module is in use" >&2
+        return 1
+    fi
+
+    rmmod efa_nv_peermem
+    return 0
+}
+
+install() {
+    echo "Unloading efa_nv_peermem kernel module"
+    _unload_driver || exit 1
+
+    until lsmod | grep -q nvidia; do
+        echo "Waiting for nvidia-driver to be installed..."
+        sleep 10
+    done
+
+    echo "Loading efa_nv_peermem kernel module"
+    modprobe -d /opt efa_nv_peermem || exit 1
+
+    echo "Done, now waiting for signal"
+    sleep infinity &
+    trap "echo 'Caught signal'; _unload_driver && { kill $!; exit 0; }" HUP INT QUIT PIPE TERM
+    trap - EXIT
+    while true; do wait $! || continue; done
+    exit 0
+}
+
+install
